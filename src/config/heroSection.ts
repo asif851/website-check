21cf81b5542de1b1bca9b391a364e6d3e6a71a49
asif851 <asifmredha21@gmail.com>
2024-09-09 23:23:00 +0600

    homepage content change

--- src/config/heroSection.ts
+++ src/config/heroSection.ts
@@ -3,8 +3,8 @@ export const heroSection = {
   HeroDescription:
     " we are committed to delivering top-tier services with a customer-first approach. Our focus is on ensuring the fastest delivery without compromising on quality. Whether you need cutting-edge data analytics, a responsive website, or a robust mobile application, Kryzotech is your go-to solution.",
   HeroButton: "Book for a Session",
-  TotalStudents: "10K+",
-  TotalCourses: "10+",
+  TotalStudents: "1K+",
+  TotalCourses: "5+",
   HeroImage:
     "https://mir-s3-cdn-cf.behance.net/project_modules/disp/17ee3d43176281.59b4059fb74a1.jpg",
 };

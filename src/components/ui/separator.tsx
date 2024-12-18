814ac7ec57192b84de7c4c86d792cf0c6a735b43
Muhammad Arnob <arnobmuhammad@gmail.com>
2024-12-18 18:30:52 +0600

    refactor: Replaced the svg with a resuable separator component.

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as SeparatorPrimitive from "@radix-ui/react-separator"
+import * as React from "react";
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const Separator = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
@@ -25,7 +25,55 @@ const Separator = React.forwardRef<
       {...props}
     />
   )
-)
-Separator.displayName = SeparatorPrimitive.Root.displayName
+);
+Separator.displayName = SeparatorPrimitive.Root.displayName;
 
-export { Separator }
+export { Separator };
+
+type SlashSeparatorProps = {
+  count?: number; // Number of slashes
+  startOffset?: number; // Starting offset position
+  step?: number; // Incremental step for spacing between each slash
+  lineLength?: number; // Length of each slash
+  orientation?: "horizontal" | "vertical";
+  className?: string; // Additional custom styling for the parent container
+};
+
+export const SlashSeparator: React.FC<SlashSeparatorProps> = ({
+  count = 24,
+  startOffset = 10,
+  step = 7,
+  lineLength = 18,
+  orientation = "horizontal",
+  className = "",
+}) => {
+  return (
+    <div
+      className={cn(orientation === "horizontal" ? "" : "rotate-90", className)}
+    >
+      <svg
+        className={cn(
+          "text-gray-300 ",
+          orientation === "horizontal" ? "w-auto h-4 lg:mx-0" : ""
+        )}
+        viewBox={`0 0 ${startOffset + step * (count - 1) + lineLength} 16`}
+        fill="none"
+        stroke="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        {Array.from({ length: count }).map((_, index) => {
+          const offset = startOffset + index * step; // Calculate the offset dynamically
+          return (
+            <line
+              key={index}
+              y1="-0.5"
+              x2={lineLength}
+              y2="-0.5"
+              transform={`matrix(-0.5547 0.83205 0.83205 0.5547 ${offset} 1)`}
+            />
+          );
+        })}
+      </svg>
+    </div>
+  );
+};
